f3458eac3c1ecdb53e4444863d71b33dbe5984bc
Subhodeep <subhodeepbasak14@gmail.com>
2024-09-17 20:39:17 +0530

    chnages for render

--- api/index.js
+++ api/index.js
@@ -4,14 +4,24 @@ import cookieParser from "cookie-parser";
 import cors from 'cors'
 import authRouter from './routes/auth.route.js'
 import todoRouter from './routes/todo.route.js'
+import path from 'path';
 const app = express();
 
 dotenv.config();
+const _dirname = path.resolve()
+
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors())
 app.use('/api/user', authRouter)
 app.use('/api/todo', todoRouter)
+
+
+app.use(express.static(path.join(_dirname, '/client/dist')));
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'))
+})
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal server error';
