b5a0b167ef48e0f1b68db7ca72ef3320ebe8f5fe
Subhodeep <subhodeepbasak14@gmail.com>
2024-09-17 19:07:28 +0530

    netlify changes

---
+++ functions/api.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from 'dotenv'
+import cookieParser from "cookie-parser";
+import cors from 'cors'
+import authRouter from '../api/routes/auth.route.js'
+import todoRouter from '../api/routes/todo.route.js'
+import serverless from 'serverless-http';
+const app = express();
+
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser())
+app.use(cors())
+app.use('/api/user', authRouter)
+app.use('/api/todo', todoRouter)
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    res.status(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    })
+})
+module.exports.handler = serverless(app);
+// app.listen(3000,()=>{
+//     console.log("Server running on port 3000")
+// })
+
